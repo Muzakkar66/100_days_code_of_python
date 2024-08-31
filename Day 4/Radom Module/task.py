6f19706675a03ce8a9813912689765b5f008756c
Muzakkar66 <muzakkir066@gmail.com>
2024-08-31 22:06:31 +0500

    added day 4

---
+++ Day 4/Radom Module/task.py
@@ -0,0 +1,28 @@
+import random
+# import my_module
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+#
+# print(my_module.my_favourite_number)
+
+# generating new floating point number
+# random_number_0_to_1 = random.random() * 10
+#
+# print(random_number_0_to_1, "hello")
+#
+# random_float = random.uniform(1, 10) # random.random() generates a random floating-point number between 0.0 and 1.0.
+#
+# print(random_float)
+
+#Making Rock Paper Soccer game
+random_heads_or_tails = random.randint(0, 1)
+print(random_heads_or_tails)
+if random_heads_or_tails == 0:
+    print("Heads")
+else:
+    print("Tails")
+
+
+
+
